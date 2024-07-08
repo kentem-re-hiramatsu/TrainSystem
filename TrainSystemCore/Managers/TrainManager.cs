1a51c2e4460e2dff109d2d5df1afdac6e79de438
re-hiramatsu <re-hiramatsu>
2024-07-08 13:23:16 +0900

    TrainManagerクラス実装

---
+++ TrainSystemCore/Managers/TrainManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TrainSystemCore.Models;
+
+namespace TrainSystemCore.Managers
+{
+    public class TrainManager
+    {
+        private List<Train> _trainDatas = new List<Train>();
+
+        public IReadOnlyCollection<Train> TrainDatas { get { return _trainDatas; } }
+
+        public void Add(Train train)
+        {
+            _trainDatas.Add(train);
+        }
+
+        public Train Get(int index)
+        {
+            return _trainDatas[index];
+        }
+
+        public int GetTotalPrice()
+        {
+            return _trainDatas.Sum(x => x.GetPrice());
+        }
+    }
+}
