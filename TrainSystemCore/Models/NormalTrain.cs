ad20d65d5d493cccebccf4af399a9eff1d45c301
re-hiramatsu <re-hiramatsu>
2024-07-08 11:44:55 +0900

    NormalTrainクラス実装

---
+++ TrainSystemCore/Models/NormalTrain.cs
@@ -0,0 +1,16 @@
+﻿using TrainSystemCore.Enums;
+
+namespace TrainSystemCore.Models
+{
+    public class NormalTrain : Train
+    {
+        private const string NAME = "普通列車";
+        private const int PRICE = 1000;
+
+        public NormalTrain(int adultCount, int childrenCount, SeatType seatType) : base(adultCount, childrenCount, seatType)
+        {
+            Name = NAME;
+            Price = PRICE;
+        }
+    }
+}
