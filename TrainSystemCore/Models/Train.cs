7df2d2240ce0c2e5138e029806d8ebb6a93c0eab
re-hiramatsu <re-hiramatsu>
2024-07-08 11:43:55 +0900

    Trainクラス実装

---
+++ TrainSystemCore/Models/Train.cs
@@ -0,0 +1,26 @@
+﻿using TrainSystemCore.Enums;
+
+namespace TrainSystemCore.Models
+{
+    public abstract class Train
+    {
+        public string Name { get; protected set; }
+        public int Price { get; protected set; }
+        public SeatType SeatType { get; }
+        public int AdultCount { get; set; }
+        public int ChildrenCount { get; set; }
+
+        public Train(int adultCount, int childrenCount, SeatType seatType)
+        {
+            AdultCount = adultCount;
+            ChildrenCount = childrenCount;
+            SeatType = seatType;
+        }
+
+        public int GetPrice()
+        {
+            int price = Price + (int)SeatType;
+            return price * AdultCount + price / 2 * ChildrenCount;
+        }
+    }
+}
