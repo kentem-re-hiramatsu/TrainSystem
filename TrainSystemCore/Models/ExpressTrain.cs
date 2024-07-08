2e673d66d0961c360c29e0d58b032793802f2a69
re-hiramatsu <re-hiramatsu>
2024-07-08 11:50:20 +0900

    ExpressTrainクラス実装

---
+++ TrainSystemCore/Models/ExpressTrain.cs
@@ -0,0 +1,16 @@
+﻿using TrainSystemCore.Enums;
+
+namespace TrainSystemCore.Models
+{
+    public class ExpressTrain : Train
+    {
+        private const string NAME = "急行列車";
+        private const int PRICE = 1500;
+
+        public ExpressTrain(int adultCount, int childrenCount, SeatType seatType) : base(adultCount, childrenCount, seatType)
+        {
+            Name = NAME;
+            Price = PRICE;
+        }
+    }
+}
