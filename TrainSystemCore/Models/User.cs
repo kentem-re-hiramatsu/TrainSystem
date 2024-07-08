61d582c4e0f2280cf8339fb444baf35c8f9e30b8
re-hiramatsu <re-hiramatsu>
2024-07-08 14:18:24 +0900

    ユーザークラスの実装

---
+++ TrainSystemCore/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TrainSystemCore.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public int AdultCount { get; set; }
+        public int ChildrenCount { get; set; }
+        public Train TrainType { get; set; }
+
+        public User(string name, int adultCount, int childrenCount, Train trainType)
+        {
+            if (adultCount > 0 && childrenCount > 0)
+            {
+                Name = name;
+                AdultCount = adultCount;
+                ChildrenCount = childrenCount;
+                TrainType = trainType;
+            }
+            else
+            {
+                throw new Exception(Consts.INPUT_COUNT_ERROR_MESSAGE);
+            }
+        }
+    }
+}
