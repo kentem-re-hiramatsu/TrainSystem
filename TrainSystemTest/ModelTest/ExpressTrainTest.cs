0971391cf4c501d194295190a58b6b19479b5077
re-hiramatsu <re-hiramatsu>
2024-07-08 11:49:59 +0900

    ExpressTrainクラスのテストケース実装

---
+++ TrainSystemTest/ModelTest/ExpressTrainTest.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrainSystemCore.Enums;
+using TrainSystemCore.Models;
+
+namespace TrainSystemTest.ModelTest
+{
+    [TestClass]
+    public class ExpressTrainTest
+    {
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            var expressTrrain = new NormalTrain(2, 3, SeatType.普通);
+
+            Assert.AreEqual(2, expressTrrain.AdultCount);
+            Assert.AreEqual(3, expressTrrain.ChildrenCount);
+            Assert.AreEqual(SeatType.普通, expressTrrain.SeatType);
+        }
+
+        [TestMethod]
+        public void SpecifiedSeatTest()
+        {
+            var expressTrrain = new NormalTrain(2, 3, SeatType.指定);
+
+            Assert.AreEqual(2, expressTrrain.AdultCount);
+            Assert.AreEqual(3, expressTrrain.ChildrenCount);
+            Assert.AreEqual(SeatType.指定, expressTrrain.SeatType);
+        }
+
+        [TestMethod]
+        public void GreenSpecifiedSeatTest()
+        {
+            var expressTrrain = new NormalTrain(2, 3, SeatType.グリーン指定);
+
+            Assert.AreEqual(2, expressTrrain.AdultCount);
+            Assert.AreEqual(3, expressTrrain.ChildrenCount);
+            Assert.AreEqual(SeatType.グリーン指定, expressTrrain.SeatType);
+        }
+    }
+}
