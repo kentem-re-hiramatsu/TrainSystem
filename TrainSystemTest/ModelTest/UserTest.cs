8d752afcf191d127892c0a9e422852ffd4e65b98
re-hiramatsu <re-hiramatsu>
2024-07-08 14:24:01 +0900

    ０以上の入力だったためUserのテスト修正

--- TrainSystemTest/ModelTest/UserTest.cs
+++ TrainSystemTest/ModelTest/UserTest.cs
@@ -12,18 +12,16 @@ namespace TrainSystemTest.ModelTest
         public void ConstructorTest()
         {
             var train = new NormalTrain(SeatType.普通);
-            var user = new User("太郎", 3, 2, train);
+            var user = new User("太郎", 0, 0, train);
 
             Assert.AreEqual("太郎", user.Name);
-            Assert.AreEqual(3, user.AdultCount);
-            Assert.AreEqual(2, user.ChildrenCount);
+            Assert.AreEqual(0, user.AdultCount);
+            Assert.AreEqual(0, user.ChildrenCount);
             Assert.IsTrue(user.TrainType is NormalTrain);
             Assert.AreEqual(SeatType.普通, user.TrainType.SeatType);
 
-            Assert.ThrowsException<Exception>(() => new User("太郎", 0, 2, train));
             Assert.ThrowsException<Exception>(() => new User("太郎", -1, 2, train));
 
-            Assert.ThrowsException<Exception>(() => new User("太郎", 2, 0, train));
             Assert.ThrowsException<Exception>(() => new User("太郎", 2, -1, train));
         }
     }
