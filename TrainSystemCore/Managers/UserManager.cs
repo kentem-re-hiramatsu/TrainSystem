9d5708b801f49074581ad9afd3532f5b9c66f3b7
re-hiramatsu <re-hiramatsu>
2024-07-08 14:34:36 +0900

    UserManagerクラス実装

---
+++ TrainSystemCore/Managers/UserManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TrainSystemCore.Models;
+
+namespace TrainSystemCore.Managers
+{
+    public class UserManager
+    {
+        private List<User> _userDatas = new List<User>();
+
+        public IReadOnlyCollection<User> UserDatas { get { return _userDatas; } }
+
+        public void Add(User user)
+        {
+            _userDatas.Add(user);
+        }
+
+        public User Get(int index)
+        {
+            return _userDatas[index];
+        }
+
+        public int GetTotalPrice()
+        {
+            return _userDatas.Sum(x => x.TrainType.GetTrainPrice() * (x.AdultCount + x.ChildrenCount / 2));
+        }
+    }
+}
