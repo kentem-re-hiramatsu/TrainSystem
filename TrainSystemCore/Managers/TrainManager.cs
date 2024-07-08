e634651f5500a11077154e8c7bdc2dc73777c639
re-hiramatsu <re-hiramatsu>
2024-07-08 15:27:04 +0900

    TrainManagerのテスト実装

---
+++ TrainSystemCore/Managers/TrainManager.cs
@@ -0,0 +1,22 @@
+﻿using TrainSystemCore.Enums;
+using TrainSystemCore.Models;
+
+namespace TrainSystemCore.Managers
+{
+    public class TrainManager
+    {
+        public Train GetTrain(TrainType trainType, SeatType seatType)
+        {
+            switch (trainType)
+            {
+                case TrainType.普通列車:
+                    return new NormalTrain(seatType);
+                case TrainType.急行列車:
+                    return new ExpressTrain(seatType);
+                case TrainType.特急列車:
+                    return new LimetedExpressTrain(seatType);
+                default: return null;
+            }
+        }
+    }
+}
---
+++ TrainSystemTest/ManagerTest/TrainManagerTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrainSystemCore.Enums;
+using TrainSystemCore.Managers;
+using TrainSystemCore.Models;
+
+namespace TrainSystemTest.ManagerTest
+{
+    [TestClass]
+    public class TrainManagerTest
+    {
+        [TestMethod]
+        public void GetTrainTest()
+        {
+            var trainMana = new TrainManager();
+            var seatType = SeatType.普通;
+
+            var normalTrain = trainMana.GetTrain(TrainType.普通列車, seatType);
+            var expressTrain = trainMana.GetTrain(TrainType.急行列車, seatType);
+            var limetedExpressTrain = trainMana.GetTrain(TrainType.特急列車, seatType);
+
+            Assert.IsTrue(normalTrain is NormalTrain);
+            Assert.IsTrue(expressTrain is ExpressTrain);
+            Assert.IsTrue(limetedExpressTrain is LimetedExpressTrain);
+        }
+    }
+}
